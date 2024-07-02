ee4558556ef4740b714150a0b296b6135c0c0fe3
Redden25 <cciara.alva@gmail.com>
2024-07-01 22:37:05 -0500

    Update Trabajo_final_v3.py

--- Trabajo_final_v3.py
+++ Trabajo_final_v3.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as py
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import plotly.express as px
 
 #Permite poner el ícono de cine en el visualizador de pestaña 
